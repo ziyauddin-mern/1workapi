0e0286392e76d6f0fc36c9c7294cc13eb074dd2a
Ziyauddin <ziyauddin7073@gmail.com>
2024-07-16 18:25:25 +0530

    scaffolding

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.listen(8080);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("success setup");
+});
